6ef81c087142f4366f4208a3dc6a9bcfcf784243
israel <itzisraelone@gmail.com>
2020-09-16 14:32:37 -0500

    Update coded using a IIFE function

--- etchasketch.js
+++ etchasketch.js
@@ -1,89 +1,84 @@
-
-var input=64;
-var randomColor=getRandomColor();
-var random=false;
-
-$(document).ready(function(){
-	grid();
-	color();
-	reset();
-	gridUpdate();
-	getRandomColor();
-	randomButton();
-});
- 
-function grid(){
-	var gridSize=input*input;
-	var pixelSize=500/input;
-
-	for(var i=0;i<gridSize;i++){
-
-		var size=$("<div class=\"pixel color\"></div>");
-		$("#mainScreen").append(size);
-
-}
-		$(".pixel").css("height", pixelSize);
- 		$(".pixel").css("width", pixelSize);
-};
-
-function color(){
-	$(".pixel").hover(function(){
-		$(this).css("background", "black")
-
-	})    	
-}
-   
-function reset(){
-	random=false;
-	$("#reset").click(function(){
-		$(".pixel").css("background", "")
-		color()
-	});
-	
-};
-
-function gridUpdate(){
-	$("#updateGrid").click(function(){
-		input=$("#input").val();
-		$("#mainScreen").empty();
-		
-		grid();
-		color();
-		reset();
-	});
-};
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    randomColor = '#';
-	
-	for (var i = 0; i < 6; i++ ) {
-	randomColor += letters[Math.round(Math.random() * 15)];
-}
-        
-  return randomColor;
-};
-    
-function randomButton(){
-
-	$("#random").click(function (){
-	
-	if(random===false){
-		random=true
-		$(".pixel").hover(function(){
-      	$(this).css("background", getRandomColor())
-  })
-  }
- 	else{
-  	random=false
-  	color()
-  }
-}
-)};
-
-
-grid();
-color();
-reset();
-getRandomColor();
-randomButton();
+(function () {
+  var grid = {
+    gridSize: this.input.value,
+    random: false,
+    init: function () {
+      this.cacheDom();
+      this.gridSetup();
+      this.bindEvents();
+      this.fillIn();
+    },
+    //caches DOM elements to be use in functions
+    cacheDom: function () {
+      this.randomButton = document.getElementById("randomButton");
+      this.resetButton = document.getElementById("resetButton");
+      this.updateGridButton = document.getElementById("updateGrid");
+      this.input = document.getElementById("input");
+      this.mainScreen = document.getElementById("mainScreen");
+      this.pixels = "";
+    },
+    //Adds even listeners to functionality buttons
+    bindEvents: function () {
+      this.randomButton.addEventListener(
+        "click",
+        this.randomColorButton.bind(this)
+      );
+      this.updateGridButton.addEventListener(
+        "click",
+        this.updateGrid.bind(this)
+      );
+    },
+    //Calculates the size of each pixel based on the grid size and input
+    gridSetup: function () {
+      let size = this.input.value;
+      let gridSize = size * size;
+      let pixelSize = 500 / size;
+      for (var i = 0; i < gridSize; i++) {
+        pixel = document.createElement("div");
+        pixel.className = "pixel";
+        pixel.style.width = pixelSize + "px";
+        pixel.style.height = pixelSize + "px";
+        this.mainScreen.append(pixel);
+      }
+    },
+    //Adds listener to each pixel, checks if their color should be black or random
+    fillIn: function () {
+      this.pixels = Array.from(document.getElementsByClassName("pixel"));
+      this.pixels.forEach(function (elem) {
+        elem.addEventListener("mouseover", function () {
+          if (grid.random === false) {
+            elem.style.background = "black";
+          } else {
+            elem.style.background = grid.getRandomColor();
+          }
+        });
+      });
+    },
+    //Turns random color on and off
+    randomColorButton: function () {
+      if (this.random === false) {
+        this.random = true;
+      } else {
+        this.random = false;
+      }
+    },
+    getRandomColor: function () {
+      var letters = "0123456789ABCDEF".split("");
+      randomColor = "#";
+      for (var i = 0; i < 6; i++) {
+        randomColor += letters[Math.round(Math.random() * 15)];
+      }
+      return randomColor;
+    },
+    //Clear and updates grid based on user's input
+    updateGrid: function () {
+      this.random = false;
+      document.querySelectorAll(".pixel").forEach(function (elem) {
+        elem.remove();
+      });
+      this.gridSetup();
+      this.fillIn();
+    },
+  };
+  grid.init();
+})();
